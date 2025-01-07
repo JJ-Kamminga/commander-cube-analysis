b160bffe8111828f3be0346834bc1beaeccaef83
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-01-07 20:16:19 +0100

    Change analysis cards to a drawer

--- src/components/Analysis/AnalysisStepCardList.tsx
+++ src/components/Analysis/AnalysisStepCardList.tsx
@@ -1,30 +1,41 @@
 import { getRandomId } from "@/utils/helpers";
 import { Card as MagicCard } from "@/utils/mtg-scripting-toolkit/scryfall";
-import { ExpandMore } from "@mui/icons-material";
-import { Accordion, AccordionDetails, AccordionSummary, List, ListItem } from "@mui/material";
+import { Button, Divider, Drawer, List, ListItem } from "@mui/material";
+import { useState } from "react";
 
 type AnalysisStepCardListProps = {
   cardNames: string[] | string[][],
 };
 
 export const AnalysisStepCardList: React.FC<AnalysisStepCardListProps> = ({ cardNames }) => {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (newOpenState: boolean) => () => {
+    setDrawerOpen(newOpenState);
+  };
+
   return (
-    <Accordion sx={{ marginBottom: '1rem' }}>
-      <AccordionSummary expandIcon={<ExpandMore />}>Click to see card names</AccordionSummary>
-      <AccordionDetails>
-        <List>
-          {cardNames.map((card) => {
+    <>
+      <Button onClick={toggleDrawer(true)}>See card names</Button>
+      <Drawer open={isDrawerOpen} onClose={toggleDrawer(false)} anchor='right'>
+        <List >
+          {cardNames.map((card, index) => {
             const cardName = Array.isArray(card)
               ? card.map((card) => card).join(' + ')
               : card
+            const indexLabel = index < 9 ? `0${index + 1}` : index + 1
             return (
-              <ListItem key={cardName + getRandomId()}>
-                {cardName}
-              </ListItem>
+              <>
+                <ListItem key={cardName + getRandomId()}>
+                  <span style={{ marginRight: '5px' }}>{indexLabel}. </span> {cardName}
+                </ListItem>
+                <Divider variant="middle" aria-hidden={true} />
+              </>
             )
           })}
         </List>
-      </AccordionDetails>
-    </Accordion>
+      </Drawer>
+
+    </>
   )
 };
