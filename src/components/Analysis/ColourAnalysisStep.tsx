9a33dec1c8c044bf6d695cc171b15606e25bfe9b
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-04-17 17:57:05 +0200

    colour identity nickname dictionary

--- src/components/Analysis/ColourAnalysisStep.tsx
+++ src/components/Analysis/ColourAnalysisStep.tsx
@@ -2,33 +2,67 @@ import { compareColourIdentities, searchColorIdentitiesByTypeLine } from "@/util
 import { AnalysisStepProps } from "./types";
 import { BarChart } from "@mui/x-charts";
 
-/** color sorting */
+const ciNicknameDictionary: { [key: string]: string } = {
+  "W": "White",
+  "U": "Blue",
+  "B": "Black",
+  "R": "Red",
+  "G": "Green",
+  "WU": "Azorius",
+  "WR": "Boros",
+  "UB": "Dimir",
+  "BG": "Golgari",
+  "RG": "Gruul",
+  "UR": "Izzet",
+  "WB": "Orzhov",
+  "BR": "Rakdos",
+  "WG": "Selesnya",
+  "UG": "Simic",
+  "WBG": "Abzan",
+  "WUG": "Bant",
+  "WUB": "Esper",
+  "UBR": "Grixis",
+  "WUR": "Jeskai",
+  "BRG": "Jund",
+  "WBR": "Mardu",
+  "WRG": "Naya",
+  "UBG": "Sultai",
+  "URG": "Temur",
+  "WBRG": "Dune",
+  "UBRG": "Glint",
+  "WURG": "Ink",
+  "WUBG": "Witch",
+  "WUBR": "Yore",
+  "WUBRG": "WUBRG",
+  "C": "Colourless"
+};
 
-// Define WUBRG order
-
-/** end color sorting */
 
 export const ColourAnalysisStep: React.FC<AnalysisStepProps> = ({ ...props }) => {
   /** todo: check whether all are needed */
   const { cardData, cubeCobraID, draftConfig, customRules } = props;
   const { playerCount, packsPerPlayer, cardsPerPack } = draftConfig;
 
-  const legendaries = searchColorIdentitiesByTypeLine(cardData, 'Legendary Creature');
-  console.log(legendaries)
-  const sortedLegendaries = legendaries.map(legendaryCI => legendaryCI.join("")).sort(compareColourIdentities)//.map(legendaryCI => legendaryCI.split(''))
-
-  function countExactStrings(arr: string[]): Record<string, number> {
-    const result: Record<string, number> = {};
-    for (const str of arr) {
-      result[str] = (result[str] || 0) + 1;
+  const legendaryColourIdentities = searchColorIdentitiesByTypeLine(cardData, 'Legendary Creature');
+  const sortedLegendaryColourIdentities = legendaryColourIdentities.map(legendaryCI => legendaryCI.join("")).sort(compareColourIdentities);
+  const analyseLegendaryColourIdentities = (colourIdentities: string[]) => {
+    const resultObj: Record<string, number> = {};
+    for (const ci of colourIdentities) {
+      resultObj[ci] = (resultObj[ci] || 0) + 1;
     }
-    return result;
-  }
+    return resultObj;
+  };
 
-
-  const data = countExactStrings(sortedLegendaries);
-  const xAxisData = Object.keys(data);
-  const series = [{ data: Object.values(data).concat() }]
+  const data = analyseLegendaryColourIdentities(sortedLegendaryColourIdentities);
+  const xAxisData = Object.keys(data).map((key => {
+    return key
+  }));
+  const series = [
+    {
+      label: 'Legendary creatures',
+      data: Object.values(data).concat(),
+    }
+  ]
 
   return (
     <>
@@ -46,7 +80,11 @@ export const ColourAnalysisStep: React.FC<AnalysisStepProps> = ({ ...props }) =>
             yAxis={[{ scaleType: 'band', data: xAxisData }]}
             series={series}
             width={1000}
-            height={500}
+            height={800}
+            barLabel={(item) => {
+              const ci = Object.keys(data)[item.dataIndex]?.toString();
+              return `${item.value} ${ci} (${ciNicknameDictionary[ci]})`;
+            }}
             layout="horizontal"
           />
         </p>
